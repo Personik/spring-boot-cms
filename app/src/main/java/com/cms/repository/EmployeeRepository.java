e1b6fb7100971964ed5528d3eca909028298c9a0
Personik <person.niklas@web.de>
2026-03-01 13:53:22 +0100

    initial commit

---
+++ app/src/main/java/com/cms/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.cms.repository;
+
+import com.cms.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+Local Setup:
+
+Copy .env_example to .env and edit accordingly
+
+Start Docker:
+
+docker compose up
